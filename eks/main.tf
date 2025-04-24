58163e1555cfd8b69edd8c0a1e7832f540f5382d
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-24 12:12:50 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -1,37 +1,31 @@
+provider "aws" {
+  region = var.region
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.8.4"
- 
 
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
-  
-  vpc_id          = var.vpc_id
-  subnet_ids      = var.subnet_ids
-  
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
 
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 1  
+      min_size     = 1
       max_size     = 3
       desired_size = 2
-      
-      
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
     }
   }
 
-    enable_irsa = true
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  enable_irsa = true
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -42,9 +36,24 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+resource "null_resource" "wait_for_eks" {
+  depends_on = [module.eks]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region} && kubectl wait --for=condition=Ready nodes --timeout=300s"
+  }
+}
+
 resource "kubernetes_namespace" "ingress_nginx" {
   metadata {
     name = "ingress-nginx"
   }
-  depends_on = [module.eks]
+
+  depends_on = [null_resource.wait_for_eks]
 }
