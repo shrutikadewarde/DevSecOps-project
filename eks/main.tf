087ad6973b455b01da668f89648cc05cb47216ab
shrutikadewarde <shrutikadewarde99@gmail.com>
2025-04-24 11:59:54 +0530

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -28,8 +28,23 @@ module "eks" {
     enable_irsa = true
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 resource "kubernetes_namespace" "ingress_nginx" {
   metadata {
     name = "ingress-nginx"
   }
+  depends_on = [module.eks]
 }
